e72c0dbe9c9865ef11fae2a7b483dd6a1faef1de
Mikhail Lisovskii <f1ste@yandex.ru>
2024-01-08 19:17:28 +0300

    create title component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,3 +1,5 @@
+import { Container } from '../../../shared/ui/Container/Container'
+import { Title, TitleSize } from '../../../shared/ui/Title/Title'
 import style from './MainPage.module.scss'
 
 
@@ -5,7 +7,12 @@ import style from './MainPage.module.scss'
 export const MainPage = () => {
 
 
-  return (
-    <>main</>
-  )
+    return (
+        <Container className={style.mainPage}>
+            <div className={style.mainPage__searchBlock}>
+                <Title className={style.mainPage__title} size={TitleSize.L}>Поск книг</Title>
+
+            </div>
+        </Container>
+    )
 }
