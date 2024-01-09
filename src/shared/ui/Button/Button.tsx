2d7e684878f00867172c575394f30e8d0cc16098
Mikhail Lisovskii <f1ste@yandex.ru>
2024-01-09 19:26:05 +0300

    implement custom button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,65 @@
+import { classNames } from 'src/shared/lib/classNames/classNames';
+import style from './Button.module.scss'
+import { ButtonHTMLAttributes, ReactNode, forwardRef } from 'react'
+import { Spinner } from '../Spinner/Spinner';
+
+export enum ButtonSize {
+  PRIMARY = '',
+}
+
+export enum ButtonTheme {
+  PRIMARY = '',
+}
+
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: ReactNode
+  size?: ButtonSize
+  theme?: ButtonTheme
+  isLoading?: boolean
+  loadingText?: string
+  contentClassname?: string
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const {
+      type,
+      className = '',
+      children,
+      size = ButtonSize.PRIMARY,
+      theme = ButtonTheme.PRIMARY,
+      isLoading,
+      disabled,
+      loadingText,
+      contentClassname = '',
+      ...otherProps
+    } = props;
+
+    const additionalClasses = [
+      className,
+      style[size],
+      style[theme]
+    ]
+
+    return (
+      <button
+        className={classNames(style.button, {}, additionalClasses)}
+        type={type ?? 'button'}
+        ref={ref}
+        disabled={isLoading || disabled}
+        {...otherProps}
+      >
+        {isLoading &&
+          <div className={style.button__loading}>
+            {loadingText ?? 'Отправка'} <Spinner />
+          </div>
+        }
+        <div className={classNames(style.button__content, { [style.isLoading]: !!isLoading }, [contentClassname])}>
+          {children}
+
+        </div>
+      </button>
+    )
+  }
+)
